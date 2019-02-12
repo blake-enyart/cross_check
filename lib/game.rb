3da4983861455d072c313c6f39cd8da18c1b14e7
Erin King <eohone@gmail.com>
2019-02-12 14:06:16 -0700

    added instance variables to Game init to reflect information in row

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,25 @@
 class Game
 
+  attr_reader :game_id,
+              :season,
+              :type
+
   def initialize(row)
+    @game_id = row[:game_id]
+    @season = row[:season]
+    @type = row[:type]
+    @date_time = row[:date_time]
+    @away_team_id = row[:away_team_id]
+    @home_team_id = row[:home_team_id]
+    @away_goals = row[:away_goals]
+    @home_goals = row[:home_goals]
+    @outcome = row[:outcome]
+    @home_rink_side_start = row[:home_rink_side_start]
+    @venue = row[:venue]
+    @venue_link = row[:venue_link]
+    @venue_time_zone_id = row[:venue_time_zone_id]
+    @venue_time_zone_offset = row[:venue_time_zone_offset]
+    @venue_time_zone_tz = row[:venue_time_zone_tz]
   end
+
 end
