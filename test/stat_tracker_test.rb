36338da2f7a415e99850522bd0c5b633b533908e
Blake Enyart <blakeenyart@Blakes-MacBook-Pro.local>
2019-02-11 17:34:41 -0700

    Create test for reading in line from game file

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -10,4 +10,18 @@ class StatTrackerTest < Minitest::Test
 
     assert_instance_of StatTracker, @stat_tracker
   end
+
+  def test_from_game_csv_extracts_line
+    game_path = './data/game.csv'
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/game_teams_stats.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    assert_equal Game, StatTracker.from_csv(locations).game_line
+  end
 end
