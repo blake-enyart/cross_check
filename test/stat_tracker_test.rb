b8d47b033ad2e0a3e6eea388a14852584dfac2ff
Blake Enyart <blakeenyart@Blakes-MacBook-Pro.local>
2019-02-11 17:09:19 -0700

    Created existence test, runner file, and stat_tracker files

---
+++ test/stat_tracker_test.rb
@@ -0,0 +1,13 @@
+require './test/test_helper'
+
+class StatTrackerTest < Minitest::Test
+
+  def setup
+    @stat_tracker = StatTracker.new
+  end
+
+  def test_it_exist
+
+    assert_instance_of StatTracker, @stat_tracker
+  end
+end
