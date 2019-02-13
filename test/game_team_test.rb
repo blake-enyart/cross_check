54dc9908e84d4bad40cee1604752882bd087d434
Blake <blake.enyart@gmail.com>
2019-02-13 16:39:26 -0700

    Edits the file path for each test file

--- test/game_team_test.rb
+++ test/game_team_test.rb
@@ -3,9 +3,9 @@
 class GameTeamTest < Minitest::Test
 
   def setup
-    game_path = './data/game_dummy.csv'
-    team_path = './data/team_info_dummy.csv'
-    game_teams_path = './data/game_teams_stats_dummy.csv'
+    game_path = './data/samples/game.csv'
+    team_path = './data/samples/team_info.csv'
+    game_teams_path = './data/samples/game_teams_stats.csv'
 
     locations = {
       games: game_path,
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -2,20 +2,19 @@ require './test/test_helper'
 
 class StatTrackerTest < Minitest::Test
 
-  # def setup
-  #   @stat_tracker = StatTracker.new()
-  # end
+  def setup
+    @stat_tracker = StatTracker.new()
+  end
 
   def test_it_exist
-    skip
 
     assert_instance_of StatTracker, @stat_tracker
   end
 
   def test_from_csv_stores_array_of_game_objects
-    game_path = './data/game_dummy.csv'
-    team_path = './data/team_info_dummy.csv'
-    game_teams_path = './data/game_teams_stats_dummy.csv'
+    game_path = './data/samples/game.csv'
+    team_path = './data/samples/team_info.csv'
+    game_teams_path = './data/samples/game_teams_stats.csv'
 
     locations = {
       games: game_path,
