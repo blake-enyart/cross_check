5c883b9eb55cbe8c852ab13a09376a420b94b7ae
Erin King <eohone@gmail.com>
2019-02-13 13:10:52 -0700

    completed GameTest to include all attributes

--- test/game_test.rb
+++ test/game_test.rb
@@ -20,8 +20,21 @@ class GameTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "2", @game.away_goals
+    assert_equal "2012030221", @game.game_id
+    assert_equal "20122013", @game.season
+    assert_equal "P", @game.type
+    assert_equal "2013-05-16", @game.date_time
+    assert_equal "3", @game.away_team_id
     assert_equal "6", @game.home_team_id
+    assert_equal "2", @game.away_goals
+    assert_equal "3", @game.home_goals
+    assert_equal "home win OT", @game.outcome
+    assert_equal "left", @game.home_rink_side_start
+    assert_equal "TD Garden", @game.venue
+    assert_equal "/api/v1/venues/null", @game.venue_link
+    assert_equal "America/New_York", @game.venue_time_zone_id
+    assert_equal "-4", @game.venue_time_zone_offset
+    assert_equal "EDT", @game.venue_time_zone_tz
   end
 
 end
