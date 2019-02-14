5f0f154ff0165021a185af21fbaa8b31f764ccb1
Erin King <eohone@gmail.com>
2019-02-14 15:08:30 -0700

    refactored StatTrackerTest tests to use set up method

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -16,71 +16,33 @@ class StatTrackerTest < Minitest::Test
     @stat_tracker = StatTracker.from_csv(@locations)
   end
 
-  def test_it_exist
-    
+  def test_it_exists
+
     assert_instance_of StatTracker, @stat_tracker
   end
 
   def test_from_csv_stores_array_of_game_objects
-    game_path = './data/samples/game.csv'
-    team_path = './data/samples/team_info.csv'
-    game_teams_path = './data/samples/game_teams_stats.csv'
 
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
-    stat_tracker = StatTracker.from_csv(locations)
-    assert_equal Game, StatTracker.from_csv(locations).games[0].class
+    assert_equal Game, StatTracker.from_csv(@locations).games[0].class
   end
 
   def test_from_csv_stores_array_of_team_objects
-    game_path = './data/samples/game.csv'
-    team_path = './data/samples/team_info.csv'
-    game_teams_path = './data/samples/game_teams_stats.csv'
 
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
-    assert_equal Team, StatTracker.from_csv(locations).teams[0].class
+    assert_equal Team, StatTracker.from_csv(@locations).teams[0].class
   end
 
   def test_from_csv_stores_array_of_game_team_objects
-    game_path = './data/samples/game.csv'
-    team_path = './data/samples/team_info.csv'
-    game_teams_path = './data/samples/game_teams_stats.csv'
 
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
-    assert_equal GameTeam, StatTracker.from_csv(locations).game_teams[0].class
+    assert_equal GameTeam, StatTracker.from_csv(@locations).game_teams[0].class
   end
 
   def test_it_can_calculate_average_goals_per_game
-    game_path = './data/samples/game.csv'
-    team_path = './data/samples/team_info.csv'
-    game_teams_path = './data/samples/game_teams_stats.csv'
 
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-    stat_tracker = StatTracker.from_csv(locations)
-
-    assert_equal 5.15, stat_tracker.average_goals_per_game
+    assert_equal 5.15, @stat_tracker.average_goals_per_game
   end
 
   def test_it_gets_count_of_teams
-    
+
     assert_equal 4, @stat_tracker.count_of_teams
   end
 
