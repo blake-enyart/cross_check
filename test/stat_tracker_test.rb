81c9a242da0241ed008dfd1a0a27c38a1ee9de17
Blake <blake.enyart@gmail.com>
2019-02-15 21:14:11 -0700

    Create test for best_season

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -134,4 +134,9 @@ class StatTrackerTest < Minitest::Test
 
     assert_equal expected, @stat_tracker.team_info("6")
   end
+
+  def test_best_season_returns_correctly
+
+    assert_equal "20122013", @stat_tracker.best_season("6")
+  end
 end
