4fc2532370fb8fa815c3fc98755b9d493a45817f
Blake <blake.enyart@gmail.com>
2019-02-18 09:40:36 -0700

    Create test for biggest_surprise method and passed test

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -221,4 +221,21 @@ class StatTrackerTest < Minitest::Test
 
     assert_equal "Sharks", stat_tracker.biggest_bust("20122013")
   end
+
+  def test_biggest_surprise_returns_correctly
+    game_path = './data/game.csv'
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/game_teams_stats.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    assert_equal "Canucks", stat_tracker.biggest_surprise("20122013")
+  end
+
 end
